898f26bbcbd491aa561fa73289a58ff98e36f99e
Piyush Mehta <me@piyushmehta.com>
2018-08-15 14:46:20 +0530

    Changes
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -6,6 +6,7 @@ import withStyles from "@material-ui/core/styles/withStyles";
 
 // @material-ui/icons
 
+
 // core components
 import Footer from "components/Footer/Footer.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
@@ -16,19 +17,21 @@ import landingPageStyle from "assets/jss/material-kit-react/views/landingPage.js
 
 // Sections for this page
 import ProductSection from "./Sections/ProductSection.jsx";
-import TeamSection from "./Sections/TeamSection.jsx";
+// import TeamSection from "./Sections/TeamSection.jsx";
+import profilePageStyle from "assets/jss/material-kit-react/views/profilePage.jsx";
 
 class LandingPage extends React.Component {
   render() {
     const { classes } = this.props;
+    const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
     return (
       <div>
         <Parallax filter image={require("assets/img/landing-bg.jpg")}>
           <div className={classes.container}>
             <GridContainer>
               <GridItem xs={12} sm={12} md={6}>
-                <h1 className={classes.title}>Wake n Bake</h1>
-                <h4>
+                <h1 style={{color:'#fff'}}>Wake n Bake</h1>
+                <h4 style={{color:"#fff"}}>
                   Order Your Food Online
                 </h4>
                 <br />
@@ -39,13 +42,43 @@ class LandingPage extends React.Component {
         <div className={classNames(classes.main, classes.mainRaised)}>
           <div className={classes.container}>
             <ProductSection />
-            <TeamSection />
           </div>
+           <GridContainer justify="center">
+            <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={4}>
+                <img
+                  alt="..."
+                  src = 'https://images.pexels.com/photos/5317/food-salad-restaurant-person.jpg'
+                  className={navImageClasses}
+                />
+                <img
+                  alt="..."
+                  src = 'https://singlestroke.io/wp-content/uploads/2015/10/high-quality-food-stock-photos-thumbnail.jpg'
+                  className={navImageClasses}
+                />
+              </GridItem>
+              <GridItem xs={12} sm={12} md={4}>
+                <img
+                  alt="..."
+                  src = 'https://st4.depositphotos.com/8509220/20462/v/600/depositphotos_204623660-stock-video-topview-shot-of-a-family.jpg'
+                  className={navImageClasses}
+                />
+                <img
+                  alt="..."
+                  src = 'https://images.parents.mdpcdn.com/sites/parents.com/files/styles/width_360/public/images/p_101198293_2.jpg'
+                  className={navImageClasses}
+                />
+              </GridItem>
+              </GridContainer>
+            </GridItem>
+          </GridContainer>
         </div>
+        {/*<TeamSection / >*/}
         <Footer />
       </div>
     );
   }
 }
 
-export default withStyles(landingPageStyle)(LandingPage);
+export default withStyles((landingPageStyle, profilePageStyle))(LandingPage);
