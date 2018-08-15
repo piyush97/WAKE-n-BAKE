108bd0acd53fe57ade90419305b90cc66044c58a
Piyush Mehta <me@piyushmehta.com>
2018-08-15 13:48:22 +0530

    Changes
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-import Components from "views/Components/Components.jsx";
+// import Components from "views/Components/Components.jsx";
 import LandingPage from "views/LandingPage/LandingPage.jsx";
 import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
 import LoginPage from "views/LoginPage/LoginPage.jsx";
